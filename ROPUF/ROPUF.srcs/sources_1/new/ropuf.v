e00fe6ae3dea8e14a9612c05b8a0fc596493e12e
Amani215 <amanibrik5@gmail.com>
2021-11-08 20:52:45 +0100

    Added: Counter for each mux

--- ROPUF/ROPUF.srcs/sources_1/new/ropuf.v
+++ ROPUF/ROPUF.srcs/sources_1/new/ropuf.v
@@ -22,10 +22,13 @@
 module ropuf(
     input enable, 
     input [1:0] sel,
-    output reg mux_output1, mux_output2
+    output[1:0] counter1, [1:0] counter2
     );
 
+//mux 1
 wire out1_1, out2_1, out3_1, out4_1;
+reg mux_output1;
+
 assign out1_1 = !(!(!(enable&out1_1)));
 assign out2_1 = !(!(!(enable&out2_1)));
 assign out3_1 = !(!(!(enable&out3_1)));
@@ -40,7 +43,10 @@ always @ (out1_1 or out2_1 or out3_1 or out4_1 or sel) begin
       endcase 
  end
  
- wire out1_2, out2_2, out3_2, out4_2;
+ //mux 2
+wire out1_2, out2_2, out3_2, out4_2;
+reg mux_output2;
+
 assign out1_2 = !(!(!(enable&out1_2)));
 assign out2_2 = !(!(!(enable&out2_2)));
 assign out3_2 = !(!(!(enable&out3_2)));
@@ -54,5 +60,31 @@ always @ (out1_2 or out2_2 or out3_2 or out4_2 or sel) begin
          2'b11 : mux_output2 = out4_2;  
       endcase 
  end  
-      
+
+//counters for each mux
+reg [1:0] counter_up1;
+reg [1:0] counter_up2;
+
+initial begin
+    counter_up1<=2'd0;
+    counter_up2<=2'd0;
+end
+
+//mux1
+always @(posedge mux_output1)
+begin
+ counter_up1 <= counter_up1 + 2'd1;
+end 
+assign counter1 = counter_up1;
+
+//mux2
+always @(posedge mux_output2)
+begin
+ counter_up2 <= counter_up2 + 2'd1;
+end 
+assign counter2 = counter_up2;
+
+//compare the counters
+//...
+
 endmodule
