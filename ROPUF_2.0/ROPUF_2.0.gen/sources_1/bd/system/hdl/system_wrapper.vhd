c80538a6c7e3d8352e98137be0d967644597b925
Amani215 <amanibrik5@gmail.com>
2021-12-01 18:28:04 +0100

    Added: ROPUF component in Microblaze

---
+++ ROPUF_2.0/ROPUF_2.0.gen/sources_1/bd/system/hdl/system_wrapper.vhd
@@ -0,0 +1,37 @@
+--Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2020.2 (win64) Build 3064766 Wed Nov 18 09:12:45 MST 2020
+--Date        : Wed Dec  1 18:18:34 2021
+--Host        : LAPTOP-5PQ60LVJ running 64-bit major release  (build 9200)
+--Command     : generate_target system_wrapper.bd
+--Design      : system_wrapper
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity system_wrapper is
+  port (
+    iclk : in STD_LOGIC;
+    uart_rxd : in STD_LOGIC;
+    uart_txd : out STD_LOGIC
+  );
+end system_wrapper;
+
+architecture STRUCTURE of system_wrapper is
+  component system is
+  port (
+    iclk : in STD_LOGIC;
+    uart_rxd : in STD_LOGIC;
+    uart_txd : out STD_LOGIC
+  );
+  end component system;
+begin
+system_i: component system
+     port map (
+      iclk => iclk,
+      uart_rxd => uart_rxd,
+      uart_txd => uart_txd
+    );
+end STRUCTURE;
